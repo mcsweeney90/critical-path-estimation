4c57f3e2e96de86d4ff867a14f0d76cd5fa4aa90
Tom <thomas.mcsweeney1990@gmail.com>
2020-09-17 18:21:15 +0100

    data analysis

--- neil_machine/analysis/benchmarking.py
+++ neil_machine/analysis/benchmarking.py
@@ -9,6 +9,7 @@ import numpy as np
 import itertools as it
 import matplotlib.pyplot as plt
 from timeit import default_timer as timer
+from collections import defaultdict
 
 ####################################################################################################
 
@@ -50,6 +51,8 @@ sizes = [100, 1000]
 n_workers = [2, 4, 8, 16]
 ccrs = [0.1, 1, 10]
 het_factors = [1.0, 2.0]
+all_param_combinations = list(it.product(*[sizes, n_workers, ccrs, het_factors]))
+dag_names = list(info[100].keys())
 
 # =============================================================================
 # First an investigation...
@@ -76,45 +79,156 @@ het_factors = [1.0, 2.0]
 # print("\nNumber of PEFT runs returning None: {}/{}".format(count, len(peft_mkspans)))
 
 """
-Ran 1000 new runs with these DAGs and parameter combinations and couldn't reproduce the error. The PEFT function returns mkspan, which is computed
-by the DAG makespan method. This uses Python's builtin max function which shouldn't ever return None, so cannot see where the problem is. Am going to 
-have to chalk this up to some unexplained bug either in the initial save or from dill. I'm satisfied that the PEFT function works in other cases and 
-since only happened for very few (3) examples out of 43,200 think it best to just disregard these cases for the moment.  
+Ran 1000 new runs with these DAGs and parameter combinations and couldn't reproduce the error. The PEFT function returns the variable mkspan, which is 
+computed by the DAG makespan method. This uses Python's builtin max function which shouldn't ever return None, so cannot see where the problem is. Am going
+to have to chalk this up to some unexplained bug either in the initial save or from dill. I'm satisfied that the PEFT function works in other cases and 
+since only happened for very few (3) examples out of 43,200 think it best to just disregard these cases.  
 """  
 
 # =============================================================================
-# Print human-readable(ish) summaries of all the information.
+# Human-readable(ish) summaries of the data.
 # =============================================================================
 
-# First a complete breakdown for all of the 2 x 4 x 3 x2 = 48 subsets.
-# This isn't particularly human-readable but useful as a reference.
-with open("{}/complete.txt".format(summary_path), "w") as dest:
-    print("COMPLETE BREAKDOWN OF HEFT AND PEFT BENCHMARKING FOR ALL SUBSETS (OF 5 x 180 = 900 DAGs) DEFINED BY PARAMETERS (n, q, CCR, h).", file=dest)
-    print("INTENDED AS A REFERENCE.", file=dest)
-    
-    dag_names = list(d for d, _ in info[100].items())    
-    for n in sizes:        
-        for q in n_workers:
-            for b in ccrs:
-                for h in het_factors:
-                    print("\nn = {}, q = {}, CCR = {}, h = {}".format(n, q, b, h), file=dest)
-                    subset_info = {}
-                    for d in dag_names:
-                        for attr in ["CP", "MST", "HEFT", "PEFT"]:
-                            try:
-                                subset_info[attr] += info[n][d][q][b][h][attr]
-                            except KeyError:
-                                subset_info[attr] = info[n][d][q][b][h][attr]
-                    # Info we want: SLRs, speedups, APDs for HEFT and PEFT. Reduction for PEFT vs HEFT. Number of failures for each. Percentage of times better...
-                    for hr in ["HEFT", "PEFT"]:
-                        print("HEURISTIC: {}".format(hr), file=dest)
-                        slrs = []
-                        for m, c in zip(subset_info[hr], subset_info["CP"]):
-                            if m is None:
-                                continue
-                            slrs.append(m/c)
-                        print("SLRs (avg, best, worst) : ({}, {}, {})".format(np.mean(slrs), min(slrs), max(slrs)), file=dest)
+def get_subset_info(info, ns, qs, bs, hs):
+    """TODO: weird bug here, had to use defaultdict to avoid..."""
+    set_info = defaultdict(list)
+    param_combinations = list(it.product(*[ns, qs, bs, hs]))
+    for n, q, b, h in param_combinations:
+        for d in dag_names:
+            for attr in ["CP", "MST", "HEFT", "PEFT"]:
+                set_info[attr] += info[n][d][q][b][h][attr]
+    #             try:
+    #                 set_info[attr] += info[n][d][q][b][h][attr]
+    #             except KeyError:
+    #                 set_info[attr] = info[n][d][q][b][h][attr]
+    return set_info    
+
+def summarize_subset(subset_info, dest):
+    for hr in ["HEFT", "PEFT"]:
+        print("HEURISTIC: {}".format(hr), file=dest)
+        slrs = []
+        for m, cp in zip(subset_info[hr], subset_info["CP"]):
+            if m is None:
+                continue
+            slrs.append(m/cp)
+        print("SLRs (avg, best, worst) : ({}, {}, {})".format(np.mean(slrs), min(slrs), max(slrs)), file=dest)
+        speedups = []
+        for m, mst in zip(subset_info[hr], subset_info["MST"]):
+            if m is None:
+                continue
+            speedups.append(mst/m)
+        print("Speedups (avg, best, worst) : ({}, {}, {})".format(np.mean(speedups), max(speedups), min(speedups)), file=dest)
+        valid = len(speedups)
+        failures = sum(1 for s in speedups if s < 1.0)
+        print("Failures (#, %): ({}, {})".format(failures, (failures/valid)*100), file=dest)
+        # Head-to-head.
+        if hr == "HEFT":
+            continue
+        reductions = []
+        for m, hm in zip(subset_info[hr], subset_info["HEFT"]):
+            if m is None:
+                continue
+            reductions.append(100 - (m/hm)*100)
+        print("Reductions (%) vs HEFT (avg, best, worst) : ({}, {}, {})".format(np.mean(reductions), max(reductions), min(reductions)), file=dest)
+        better = sum(1 for r in reductions if r > 0)
+        same = sum(1 for r in reductions if abs(r) < 1e-6)
+        worse = sum(1 for r in reductions if r < 0)
+        print("Comparison vs HEFT (%better, %same, %worse): ({}, {}, {})".format((better/valid)*100, (same/valid)*100, (worse/valid)*100), file=dest)    
+
+# Individual summaries for all 2 x 4 x 3 x 2 = 48 subsets (intended mostly as a reference since not easy to parse.)
+# with open("{}/complete.txt".format(summary_path), "w") as dest:
+#     print("HEFT AND PEFT BENCHMARKING FOR ALL SUBSETS (OF 5 x 180 = 900 DAGs) DEFINED BY PARAMETERS (n, q, CCR, h).", file=dest) 
+#     for n, q, b, h in all_param_combinations:
+#         print("\nn = {}, q = {}, CCR = {}, h = {}".format(n, q, b, h), file=dest)
+#         subset_info = get_subset_info(info, ns=[n], qs=[q], bs=[b], hs=[h])
+#         summarize_subset(subset_info, dest)
+# By size.          
+with open("{}/size_breakdown.txt".format(summary_path), "w") as dest:
+    print("HEFT AND PEFT BENCHMARKING BROKEN DOWN BY DAG SIZE n.", file=dest)     
+    for n in sizes:
+        print("\nn = {}".format(n))
+        print("\nn = {}".format(n), file=dest)
+        print("-----------------------------", file=dest)
+        subset_info = get_subset_info(info, ns=[n], qs=n_workers, bs=ccrs, hs=het_factors)
+        print(len(subset_info["CP"]))
+        summarize_subset(subset_info, dest)        
+# By number of processors.          
+with open("{}/processor_number_breakdown.txt".format(summary_path), "w") as dest:
+    print("HEFT AND PEFT BENCHMARKING BROKEN DOWN BY NUMBER OF PROCESSORS q.", file=dest)     
+    for q in n_workers:
+        print("\nq = {}".format(q))
+        print("\nq = {}".format(q), file=dest)
+        print("-----------------------------", file=dest)
+        subset_info = get_subset_info(info, ns=sizes, qs=[q], bs=ccrs, hs=het_factors)
+        print(len(subset_info["CP"]))
+        summarize_subset(subset_info, dest)
+# By CCR.          
+with open("{}/ccr_breakdown.txt".format(summary_path), "w") as dest:
+    print("HEFT AND PEFT BENCHMARKING BROKEN DOWN BY CCR.", file=dest)     
+    for b in ccrs:
+        print("\nCCR = {}".format(b))
+        print("\nCCR = {}".format(b), file=dest)
+        print("-----------------------------", file=dest)
+        subset_info = get_subset_info(info, ns=sizes, qs=n_workers, bs=[b], hs=het_factors)
+        print(len(subset_info["CP"]))
+        summarize_subset(subset_info, dest)
+# By het factor.          
+with open("{}/het_factor_breakdown.txt".format(summary_path), "w") as dest:
+    print("HEFT AND PEFT BENCHMARKING BROKEN DOWN BY HETEROGENEITY FACTOR h.", file=dest)     
+    for h in het_factors:
+        print("\nh = {}".format(h))
+        print("\nh = {}".format(h), file=dest)
+        print("-----------------------------", file=dest)
+        subset_info = get_subset_info(info, ns=sizes, qs=n_workers, bs=ccrs, hs=[h])
+        print(len(subset_info["CP"]))
+        summarize_subset(subset_info, dest)
         
+    
+        
+    
+  
+        
+    
+#     for n, q, b, h in param_combinations:
+#         print("\nn = {}, q = {}, CCR = {}, h = {}".format(n, q, b, h), file=dest)
+#         subset_info = {}
+#         for d in dag_names:
+#             for attr in ["CP", "MST", "HEFT", "PEFT"]:
+#                 try:
+#                     subset_info[attr] += info[n][d][q][b][h][attr]
+#                 except KeyError:
+#                     subset_info[attr] = info[n][d][q][b][h][attr]
+#         # Summarise all info for the subset. 
+#         for hr in ["HEFT", "PEFT"]:
+#             print("HEURISTIC: {}".format(hr), file=dest)
+#             slrs = []
+#             for m, cp in zip(subset_info[hr], subset_info["CP"]):
+#                 if m is None:
+#                     continue
+#                 slrs.append(m/cp)
+#             print("SLRs (avg, best, worst) : ({}, {}, {})".format(np.mean(slrs), min(slrs), max(slrs)), file=dest)
+#             speedups = []
+#             for m, mst in zip(subset_info[hr], subset_info["MST"]):
+#                 if m is None:
+#                     continue
+#                 speedups.append(mst/m)
+#             print("Speedups (avg, best, worst) : ({}, {}, {})".format(np.mean(speedups), max(speedups), min(speedups)), file=dest)
+#             valid = len(speedups)
+#             failures = sum(1 for s in speedups if s < 1.0)
+#             print("Failures (#, %): ({}, {})".format(failures, (failures/valid)*100), file=dest)
+#             # Head-to-head.
+#             if hr == "HEFT":
+#                 continue
+#             reductions = []
+#             for m, hm in zip(subset_info[hr], subset_info["HEFT"]):
+#                 if m is None:
+#                     continue
+#                 reductions.append(100 - (m/hm)*100)
+#             print("Reductions (%) vs HEFT (avg, best, worst) : ({}, {}, {})".format(np.mean(reductions), max(reductions), min(reductions)), file=dest)
+#             better = sum(1 for r in reductions if r > 0)
+#             same = sum(1 for r in reductions if abs(r) < 1e-6)
+#             worse = sum(1 for r in reductions if r < 0)
+#             print("Comparison vs HEFT (%better, %same, %worse): ({}, {}, {})".format((better/valid)*100, (same/valid)*100, (worse/valid)*100), file=dest)
 
 
 
